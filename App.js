c9e0614e74685370be213beb889b196d65a436ec
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-11 12:04:12 +0000

    completed today's tasks for this repo

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 // Import data and WeatherCard here
-
+const cities = require("./data");
+const WeatherCard = require("./components/WeatherCard");
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((city) => (
+          <WeatherCard data={city} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
